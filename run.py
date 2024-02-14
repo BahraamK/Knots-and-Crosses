7b68da2d4116230d28891ed7824c7cf7a363b378
BahraamK <>
2024-02-14 04:01:23 +0000

    add code to define win conditions

--- run.py
+++ run.py
@@ -6,3 +6,25 @@ def display():
     print('[' + game_board[3] + ']' + '[' + game_board[4] + ']' + '[' + game_board[5] + ']')
     print('[' + game_board[6] + ']' + '[' + game_board[7] + ']' + '[' + game_board[8] + ']')
 
+def check():
+    # Check for a win condition for either player
+    player1 = 'x'
+    player2 = 'o'
+    if game_board[0] == game_board[1] == game_board[2] == player1 or game_board[0] == game_board[1] == game_board[2] == player2:
+        return True
+    elif game_board[3] == game_board[4] == game_board[5] == player1 or game_board[3] == game_board[4] == game_board[5] == player2:
+        return True
+    elif game_board[6] == game_board[7] == game_board[8] == player1 or game_board[6] == game_board[7] == game_board[8] == player2:
+        return True
+    elif game_board[3] == game_board[0] == game_board[6] == player1 or game_board[3] == game_board[0] == game_board[6] == player2:
+        return True
+    elif game_board[1] == game_board[4] == game_board[7] == player1 or game_board[1] == game_board[4] == game_board[7] == player2:
+        return True
+    elif game_board[2] == game_board[5] == game_board[8] == player1 or game_board[2] == game_board[5] == game_board[8] == player2:
+        return True
+    elif game_board[0] == game_board[4] == game_board[8] == player1 or game_board[0] == game_board[4] == game_board[8] == player2:
+        return True
+    elif game_board[2] == game_board[4] == game_board[6] == player1 or game_board[2] == game_board[4] == game_board[6] == player2:
+        return True        
+    else:
+        return False
