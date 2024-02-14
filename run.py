0ef7c97e5b2592cff999e722b4004b7db954e0d8
BahraamK <>
2024-02-14 04:19:30 +0000

    fix input function for deployment

--- run.py
+++ run.py
@@ -14,32 +14,32 @@ def check():
         return True
     elif game_board[3] == game_board[4] == game_board[5] == player1 or game_board[3] == game_board[4] == game_board[5] == player2:
         return True
-    elif game_board[6] == game_board[7] == game_board[8] == player1 or game_board[6] == game_board[7] == game_board[8] == player2:
-        return True
     elif game_board[3] == game_board[0] == game_board[6] == player1 or game_board[3] == game_board[0] == game_board[6] == player2:
+        return True
+    elif game_board[6] == game_board[7] == game_board[8] == player1 or game_board[6] == game_board[7] == game_board[8] == player2:
         return True
     elif game_board[1] == game_board[4] == game_board[7] == player1 or game_board[1] == game_board[4] == game_board[7] == player2:
         return True
     elif game_board[2] == game_board[5] == game_board[8] == player1 or game_board[2] == game_board[5] == game_board[8] == player2:
         return True
-    elif game_board[0] == game_board[4] == game_board[8] == player1 or game_board[0] == game_board[4] == game_board[8] == player2:
-        return True
     elif game_board[2] == game_board[4] == game_board[6] == player1 or game_board[2] == game_board[4] == game_board[6] == player2:
-        return True        
+        return True  
+    elif game_board[0] == game_board[4] == game_board[8] == player1 or game_board[0] == game_board[4] == game_board[8] == player2:
+        return True      
     else:
         return False
 
 def input_position():
     # Allows for only valid input position from the user
-    x = int(input("Enter the position:"))
+    x = int(input("Enter the position:\n"))
     if game_board[x - 1] != '-':
         print("Value already exists, please enter a new value:")
         return input_position()
     else:
         return x
 
-player1 = input("Player name 1: ")
-player2 = input("Player name 2: ")
+player1 = input("Player name 1: \n")
+player2 = input("Player name 2: \n")
 
 for i in range(9):
     if i % 2 == 0  :
